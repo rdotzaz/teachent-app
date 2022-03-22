ca0d1c87a44806269c7e8492194691ce9e633ee3
rdotzaz <radek.zaz.1999@gmail.com>
2022-03-22 22:44:57 +0100

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:teachent_app/database/database.dart';
 import 'package:teachent_app/view/main_app.dart';
