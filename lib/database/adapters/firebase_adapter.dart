c180cc31cffc46fc98f92914de5e23a2162850d9
rdotzaz <radek.zaz.1999@gmail.com>
2022-03-23 22:02:03 +0100

    another try to resolve analysis error

--- lib/database/adapters/firebase_adapter.dart
+++ lib/database/adapters/firebase_adapter.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
-import 'package:teachent_app/common/firebase_options.dart'; // uri_does_not_exist
+import 'package:teachent_app/common/firebase_options.dart'; // ignore: uri_does_not_exist
 import 'package:teachent_app/database/database.dart';
 
 import '../../common/consts.dart' show DatabaseConsts;
@@ -19,8 +19,8 @@ class FirebaseRealTimeDatabaseAdapter {
 
   static Future<void> _startDataBase(DBMode dbMode) async {
     var firebaseOptions = defaultTargetPlatform == TargetPlatform.android
-        ? androidFirebaseOption // undefined_identifier
-        : webFirebaseOption; // undefined_identifier
+        ? androidFirebaseOption // ignore: undefined_identifier
+        : webFirebaseOption; // ignore: undefined_identifier
 
     await Firebase.initializeApp(options: firebaseOptions);
 
