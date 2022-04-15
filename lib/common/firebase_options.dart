a61702e4ae6647092b545704641e863da300dcfa
rdotzaz <radek.zaz.1999@gmail.com>
2022-04-15 13:43:14 +0200

    add firebase options for release version

---
+++ lib/common/firebase_options.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const webFirebaseOption = FirebaseOptions(
+    apiKey: 'AIzaSyCkhyM1Ebo6LhY0kWe2s9ed70hqoJb5NHo',
+    authDomain: 'teachentapp.firebaseapp.com',
+    databaseURL:
+        'https://teachentapp-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'teachentapp',
+    storageBucket: 'teachentapp.appspot.com',
+    messagingSenderId: '472666586305',
+    appId: '1:472666586305:web:834e35e0a30111587a2840',
+    measurementId: 'G-N1R9WC63JZ');
+
+const androidFirebaseOption = FirebaseOptions(
+    apiKey: 'AIzaSyCxqK28vbsmVe3aDWFqiKYhdqTNbvOO2Vo',
+    databaseURL:
+        'https://teachentapp-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'teachentapp',
+    storageBucket: 'teachentapp.appspot.com',
+    messagingSenderId: '472666586305',
+    appId: '1:472666586305:android:a63bc940607d07217a2840');
