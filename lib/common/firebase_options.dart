088f4edcd254896685d1a0e08e0986f843f13b1a
Radek Zazulczak <radek.zaz.1999@gmail.com>
2022-06-19 15:19:39 +0200

    Remove real db keys (#33)
    * remove real keys
    
    * add google-services template

--- lib/common/firebase_options.dart
+++ lib/common/firebase_options.dart
@@ -1,27 +1,24 @@
 import 'package:firebase_core/firebase_core.dart';
 
 /// Firebase options for web app required to access database
-/// IMPORTANT: These data should not be reveal in public repo
 ///
 /// Note: Web app required only for quick testing
 const webFirebaseOption = FirebaseOptions(
-    apiKey: 'AIzaSyCkhyM1Ebo6LhY0kWe2s9ed70hqoJb5NHo',
-    authDomain: 'teachentapp.firebaseapp.com',
+    apiKey: 'WEB_API_KEY',
+    authDomain: 'AUTH_DOMAIN',
     databaseURL:
-        'https://teachentapp-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'teachentapp',
-    storageBucket: 'teachentapp.appspot.com',
-    messagingSenderId: '472666586305',
-    appId: '1:472666586305:web:834e35e0a30111587a2840',
-    measurementId: 'G-N1R9WC63JZ');
+        'DATABASE_URL',
+    projectId: 'PROJECT_ID',
+    storageBucket: 'STORAGE_BUCKET',
+    messagingSenderId: 'MESSAGING_ID',
+    appId: 'WEB_API_ID');
 
 /// Firebase options for android app required to access database
-/// IMPORTANT: These data should not be reveal in public repo
 const androidFirebaseOption = FirebaseOptions(
-    apiKey: 'AIzaSyCxqK28vbsmVe3aDWFqiKYhdqTNbvOO2Vo',
+    apiKey: 'ANDROID_API_KEY',
     databaseURL:
-        'https://teachentapp-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'teachentapp',
-    storageBucket: 'teachentapp.appspot.com',
-    messagingSenderId: '472666586305',
-    appId: '1:472666586305:android:a63bc940607d07217a2840');
+        'DATABASE_URL',
+    projectId: 'PROJECT_ID',
+    storageBucket: 'STORAGE_BUCKET',
+    messagingSenderId: 'MESSAGING_ID',
+    appId: 'ANDROID_API_ID');
